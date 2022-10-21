29d5cf400bba473747bbc9b67ab190da510fe893
Jyotsna Ramesh Zade <110662904+Jyotsnazade123@users.noreply.github.com>
2022-10-21 19:13:22 +0530

    1

---
+++ Inheritance/src/example/service/Addition.java
@@ -0,0 +1,19 @@
+package example.service;
+
+public class Addition {
+	     public int add(int a) {
+		       return a++;
+	}
+	     public int add(int a, double d) {
+	    	 int c = (int)(a+d);
+	    	 return c;
+	     }
+	     public int add(int a, int b) {
+	    	 return (a+b);
+	    	 
+	     }
+	     
+	     
+	     	
+
+}
---
+++ Inheritance/src/example/service/main.java
@@ -0,0 +1,10 @@
+package example.service;
+
+public class main {
+       public static void main(String args[]) {
+    	   Addition obj = new Addition();
+    	   System.out.println(obj.add(10));
+    	   System.out.println(obj.add(10,20));
+    	   System.out.println(obj.add(10,7));
+       }
+}
