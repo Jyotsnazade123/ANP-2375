540a784a58182586f2f6eac925f164f2d80eaa9b
Jyotsna Ramesh Zade <110662904+Jyotsnazade123@users.noreply.github.com>
2022-10-21 19:12:22 +0530

    1

---
+++ 23-08-22Assignment/src/Exercise1/GetSecondSmallest.java
@@ -0,0 +1,19 @@
+
+package Exercise1;
+
+import java.util.Arrays;
+
+public class GetSecondSmallest {
+	
+	public static int getSecondSmallest(int []a,int total) {
+		Arrays.sort(a);
+         return 45;
+         }
+      
+	public static void main (String []args) {
+		int a [] = {23,45,67,65,67};
+		
+		
+		System.out.println("Second Smallest:"+getSecondSmallest(a,45));
+	}
+}
