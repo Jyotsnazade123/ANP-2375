a1a65050dbb182101a74ead5e96ad7ca02d67816
Jyotsna Ramesh Zade <110662904+Jyotsnazade123@users.noreply.github.com>
2022-10-29 11:29:30 +0530

    1

---
+++ practicaltest_27_10_22.sql
@@ -0,0 +1,14 @@
+create database practicaltest_27_10_22;
+use practicaltest_27_10_22;
+
+create table Employee(
+id int,
+name varchar(45),
+phone varchar(10),
+address varchar(150),
+email varchar(25),
+dept varchar(25)
+);
+
+select * from Employee;
+
